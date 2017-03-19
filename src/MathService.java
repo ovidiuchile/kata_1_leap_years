c87fab10556d8d4b50e6f49cfc3aa50a5e85d93e
Ovidiu CHILE <C:\Users\ochile\AppData\Roaming\The Bat!>
2017-03-20 00:35:00 +0200

    Refactored the code

--- src/MathService.java
+++ src/MathService.java
@@ -1,11 +1,11 @@
 public class MathService {
-    public static boolean isLeapYear(int year) {
-        if (year % 400 == 0)
-        	return true;
-    	if (year % 4 != 0)
-        	return false;
-        if (year % 100 == 0)
-        	return false;
-        return true;
-    }
+	public static boolean isLeapYear(int year) {
+		if (year % 400 == 0)
+			return true;
+		if (year % 4 != 0)
+			return false;
+		if (year % 100 == 0)
+			return false;
+		return true;
+	}
 }
