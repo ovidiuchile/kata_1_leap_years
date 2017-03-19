c6a42b53e1ab7ddb2f2f7831bd588a442f0a9603
Ovidiu CHILE <C:\Users\ochile\AppData\Roaming\The Bat!>
2017-03-20 00:32:27 +0200

    The testing methods run correctly

--- src/MathService.java
+++ src/MathService.java
@@ -1,6 +1,8 @@
 public class MathService {
     public static boolean isLeapYear(int year) {
-        if (year % 4 != 0)
+        if (year % 400 == 0)
+        	return true;
+    	if (year % 4 != 0)
         	return false;
         if (year % 100 == 0)
         	return false;
